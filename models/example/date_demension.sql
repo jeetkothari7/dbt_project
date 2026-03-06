858194e5318d068d3c6ea229dca4ba727e0f83da
Jeet Kothari <jeetkothari4@gmail.com>
2026-03-06 21:35:33 +0000

    add daily_weather demension

--- models/example/date_demension.sql
+++ models/example/date_demension.sql
@@ -3,14 +3,8 @@ with cte as (
     to_timestamp(STARTED_AT) AS STARTED_AT,
     DATE(to_timestamp(STARTED_AT)) AS DATE_STARTED_AT,
     HOUR(to_timestamp(STARTED_AT)) AS HOUR_STARTED_AT,
-    CASE WHEN DAYNAME(to_timestamp(STARTED_AT)) IN ('sat','sun') 
-    THEN 'WEEKEND' 
-    ELSE 'WEEKDAY'
-    END AS DAY_TYPE,
-    CASE WHEN MONTH(to_timestamp(STARTED_AT)) IN (12,1,2) THEN 'SPRING'
-         WHEN MONTH(to_timestamp(STARTED_AT)) IN (3,4,5) THEN 'SUMMER'
-         ELSE 'AUTUMN'
-         END AS STATION_OF_YEAR
+    {{day_type('STARTED_AT')}} AS DAY_TYPE,
+    {{get_season('STARTED_AT')}} AS STATION_OF_YEAR
     from {{ source('demo', 'bike') }}
     where STARTED_AT != 'started_at'
 )
